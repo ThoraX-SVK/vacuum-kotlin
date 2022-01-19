481cb5a5af71a3ce772623485103c1af75c4ee9e
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-19 13:13:04 +0100

    Initialize Spring Boot project

---
+++ src/main/kotlin/com/vacuumlabs/learning/KotlinDemoApplication.kt
@@ -0,0 +1,14 @@
+package com.vacuumlabs.learning
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication {
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDemoApplication>(*args)
+}
