942749acb8f0300c6e56f77c3abf3d89cc49e988
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-19 15:53:55 +0100

    Add ingredient runner to populate DB
    Also move runners into separate directory

--- src/main/kotlin/com/vacuumlabs/learning/startup/runners/FinishRunner.kt
+++ src/main/kotlin/com/vacuumlabs/learning/startup/runners/FinishRunner.kt
@@ -1,5 +1,6 @@
-package com.vacuumlabs.learning.startup
+package com.vacuumlabs.learning.startup.runners
 
+import com.vacuumlabs.learning.startup.OrderedCommandLineRunner
 import org.springframework.stereotype.Component
 
 
