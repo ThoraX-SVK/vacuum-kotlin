ce1a3720f3a3f8ea6ea1270c1fba56c8caec7e35
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-20 19:40:38 +0100

    Add support for food tags
    Based on food tags, in the future we will be able to query
    food only with some certain tags.

---
+++ src/main/kotlin/com/vacuumlabs/learning/controller/FoodTagController.kt
@@ -0,0 +1,11 @@
+package com.vacuumlabs.learning.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/food/tag")
+class FoodTagController {
+
+
+}
--- src/main/kotlin/com/vacuumlabs/learning/startup/runners/DatabasePopulationRunner.kt
+++ src/main/kotlin/com/vacuumlabs/learning/startup/runners/DatabasePopulationRunner.kt
@@ -1,21 +1,27 @@
 package com.vacuumlabs.learning.startup.runners
 
 import com.vacuumlabs.learning.food.Food
+import com.vacuumlabs.learning.food.tag.FoodTag
 import com.vacuumlabs.learning.ingredient.Ingredient
 import com.vacuumlabs.learning.ingredient.IngredientUnitType
 import com.vacuumlabs.learning.repository.FoodRepository
+import com.vacuumlabs.learning.repository.FoodTagRepository
 import com.vacuumlabs.learning.startup.OrderedCommandLineRunner
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
 class DatabasePopulationRunner @Autowired constructor(
-    val foodRepository : FoodRepository
+    val foodRepository : FoodRepository,
+    val foodTagRepository: FoodTagRepository
 ) : OrderedCommandLineRunner() {
 
     override fun run(vararg args: String?) {
 
-        val food : Food = Food("toast", mutableListOf<Ingredient>())
+        val tag = FoodTag("Breakfast")
+        foodTagRepository.save(tag)
+
+        val food : Food = Food("toast", mutableListOf(), mutableSetOf(tag))
         val toastBread = Ingredient("Toast bread bun", IngredientUnitType.PCS, 1, food)
         val butter = Ingredient("Butter", IngredientUnitType.PCS, 1, food)
         val ham = Ingredient("Ham", IngredientUnitType.PCS, 1, food)
