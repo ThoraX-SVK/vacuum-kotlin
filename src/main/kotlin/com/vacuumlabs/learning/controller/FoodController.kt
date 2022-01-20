7995af35a8b15b488f906e20257c29aef261cb75
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-20 15:51:26 +0100

    Add edit food endpoint

--- src/main/kotlin/com/vacuumlabs/learning/controller/FoodController.kt
+++ src/main/kotlin/com/vacuumlabs/learning/controller/FoodController.kt
@@ -4,9 +4,11 @@ import com.vacuumlabs.learning.food.Food
 import com.vacuumlabs.learning.repository.FoodRepository
 import com.vacuumlabs.learning.service.FoodService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/food")
@@ -24,6 +26,15 @@ class FoodController @Autowired constructor(
         foodService.isFoodFilledValidly(food)
         val savedFood = foodService.saveFood(food);
         return ResponseEntity.ok(savedFood)
+    }
+
+    @RequestMapping(value = ["/edit/{foodId}"], method = [RequestMethod.PUT], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun editFood(@PathVariable foodId : Int, @RequestBody food : Food) {
+        foodService.isFoodFilledValidly(food)
+        val persistedFood = foodService.findOne(foodId).orElseThrow { throw ResponseStatusException(HttpStatus.NOT_FOUND, "No food with given ID.") }
+        food.id = persistedFood.id
+
+        foodService.saveFood(food)
     }
 
     @RequestMapping(value = ["/delete/{foodId}"], method = [RequestMethod.DELETE])
