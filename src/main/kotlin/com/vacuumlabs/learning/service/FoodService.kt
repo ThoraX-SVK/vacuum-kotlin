5a35d8fb55385163d98e4bf0bf6cea2e63dde2f9
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-20 15:51:26 +0100

    Add edit food endpoint

--- src/main/kotlin/com/vacuumlabs/learning/service/FoodService.kt
+++ src/main/kotlin/com/vacuumlabs/learning/service/FoodService.kt
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import org.springframework.web.server.ResponseStatusException
+import java.util.*
 
 @Service
 class FoodService @Autowired constructor(
@@ -32,6 +33,10 @@ class FoodService @Autowired constructor(
     fun saveFood(food : Food) : Food {
         food.ingredients.forEach { it.food = food }
         return foodRepository.save(food)
+    }
+
+    fun findOne(foodId : Int) : Optional<Food> {
+        return foodRepository.findById(foodId)
     }
 
     fun deleteFood(foodId : Int) {
