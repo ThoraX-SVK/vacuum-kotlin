3a5b264a9c09f26431684d295922f805e7da50d0
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-20 10:35:33 +0100

    Populate DB with food
    Change the default for DB in application.properties for create. On every run it drops DB,
    so it's clean every run.

---
+++ src/main/kotlin/com/vacuumlabs/learning/startup/runners/DatabasePopulationRunner.kt
@@ -0,0 +1,31 @@
+package com.vacuumlabs.learning.startup.runners
+
+import com.vacuumlabs.learning.food.Food
+import com.vacuumlabs.learning.ingredient.Ingredient
+import com.vacuumlabs.learning.ingredient.IngredientCountType
+import com.vacuumlabs.learning.repository.FoodRepository
+import com.vacuumlabs.learning.startup.OrderedCommandLineRunner
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class DatabasePopulationRunner @Autowired constructor(
+    val foodRepository : FoodRepository
+) : OrderedCommandLineRunner() {
+
+    override fun run(vararg args: String?) {
+
+        val food : Food = Food("toast", mutableListOf<Ingredient>())
+        val toastBread = Ingredient("Toast bread bun", IngredientCountType.BOOLEAN, 1, food)
+        val butter = Ingredient("Butter", IngredientCountType.BOOLEAN, 1, food)
+        val ham = Ingredient("Ham", IngredientCountType.BOOLEAN, 1, food)
+        val cheese = Ingredient("Cheddar cheese", IngredientCountType.BOOLEAN, 1, food)
+
+        food.ingredients.add(toastBread)
+        food.ingredients.add(butter)
+        food.ingredients.add(ham)
+        food.ingredients.add(cheese)
+
+        foodRepository.save(food)
+    }
+}
