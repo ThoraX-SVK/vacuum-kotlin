7ddcfc98fb0f1813d74a734eeece943763f348a9
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-20 14:33:55 +0100

    Add functionality to post new food object through REST
    Changed controller to only ask service for data, dependency for repository is
    only in service.

--- src/main/kotlin/com/vacuumlabs/learning/controller/FoodController.kt
+++ src/main/kotlin/com/vacuumlabs/learning/controller/FoodController.kt
@@ -2,9 +2,11 @@ package com.vacuumlabs.learning.controller
 
 import com.vacuumlabs.learning.food.Food
 import com.vacuumlabs.learning.repository.FoodRepository
+import com.vacuumlabs.learning.service.FoodService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Controller
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
@@ -12,11 +14,19 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/food")
 class FoodController @Autowired constructor(
-    val foodRepository : FoodRepository
+    val foodService: FoodService,
 ) {
 
     @RequestMapping(value = ["/all"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getAllFood() : List<Food> {
-        return foodRepository.findAll()
+    fun getAllFood(): List<Food> {
+        return foodService.getAll()
     }
+
+    @RequestMapping(value = ["/add"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
+    fun addNewFood(@RequestBody food: Food): ResponseEntity<*> {
+        foodService.isFoodFilledValidly(food)
+        val savedFood = foodService.saveFood(food);
+        return ResponseEntity.ok(savedFood)
+    }
+
 }
---
+++ src/main/kotlin/com/vacuumlabs/learning/service/FoodService.kt
@@ -0,0 +1,41 @@
+package com.vacuumlabs.learning.service
+
+import com.vacuumlabs.learning.food.Food
+import com.vacuumlabs.learning.ingredient.Ingredient
+import com.vacuumlabs.learning.repository.FoodRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
+
+@Service
+class FoodService @Autowired constructor(
+    val foodRepository : FoodRepository
+) {
+
+    fun getAll() : List<Food> {
+        return foodRepository.findAll()
+    }
+
+    fun isFoodFilledValidly(food: Food) {
+        if(food.name.isBlank()) { throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Food name must consist of at least one character!") }
+        if(food.ingredients.isEmpty()) { throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Food needs to have at least one ingredient!") }
+
+        food.ingredients.forEachIndexed { index, ingredient ->  isIngredientFilledValidly(ingredient, index) }
+    }
+
+    private fun isIngredientFilledValidly(ingredient : Ingredient, index: Int) {
+        if(ingredient.name.isBlank()) { throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Ingredient at position ${index} - Name must consist of at least one character!") }
+        if(ingredient.amount <= 0) { throw ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, "Ingredient at position ${index} - Amount must be positive!") }
+    }
+
+    fun saveFood(food : Food) : Food {
+        food.ingredients.forEach { it.food = food }
+        return foodRepository.save(food)
+    }
+
+
+
+
+
+}
