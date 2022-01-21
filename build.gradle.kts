97f5c6ccd5522d04209a7227ad81b8988ad705ef
Tomas Krason <tomas.krason@vacuumlabs.com>
2022-01-21 15:36:01 +0100

    Add tests for persisting food to DB
    Add check for duplicity of ingredients, as that is not allowed and exception
    must be thrown (on frontend check would be made for this)
    
    Add profile to runners, so they are not being run when testing as they populate
    database and that's not needed during testing (each test would populate DB
    by themselves, if it would be needed)

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.zonky.test:embedded-database-spring-test:2.1.1")
 }
 
 tasks.withType<KotlinCompile> {
